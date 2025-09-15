1711ff3da69bf177ba836b16d6facce23e056f25
Quynh <quynhfe99@gmail.com>
2025-09-16 00:02:53 +0700

    fix bg image of header and hero

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -7,7 +7,7 @@ const aboutUsDescription =
 function AboutUs() {
   return (
     <div className=' bg-(--color-bg-secondary)  w-full lg:h-400 h-fit  '>
-      <div className='h-fit lg:h-400  w-full  block lg:top-0.75 bg-[--color-bg-secondary] relative  px-(--mx-sm) md:px-(--mx-md) lg:px-(--mx-lg) xl:px-(--mx-xl)'>
+      <div className='h-fit lg:h-400  w-full  block l bg-[--color-bg-secondary] relative  px-(--mx-sm) md:px-(--mx-md) lg:px-(--mx-lg) xl:px-(--mx-xl)'>
         <div className='introdution  w-full relative  max-w-7xl mx-auto  lg:sticky top-0 bottom-0 z-1'>
           <h2 className='introdution-title'>(About us)</h2>
           <Paragraph value={aboutUsDescription} />
